d602cc54b4c3f97ca7f4913935ef2bd2f3fdda53
mr. whalen <michael.whalen@sitechhs.com>
2025-02-25 12:21:08 -0500

    dfghdgf

---
+++ appT/src/views/PokemonData.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ pokemon.name }}</h2>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const pokemon = ref("");
+async function getPokemon() {
+  let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.id}`);
+  let data = await res.json();
+  pokemon.value = data;
+}
+
+onMounted(() => {
+  getPokemon();
+});
+</script>
+
+<style scoped></style>
