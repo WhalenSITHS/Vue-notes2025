81e8900ccae41b6fe438083b692a5d029e8b93a9
mr. whalen <michael.whalen@sitechhs.com>
2025-05-27 11:42:16 -0400

    dfgh

--- app2/src/views/HomeView.vue
+++ app2/src/views/HomeView.vue
@@ -4,10 +4,12 @@
     <h2>{{ user }}</h2>
     <input type="text" v-model="user" />
     <button @click="increment">Click Me</button>
+    <HelloWorld />
   </div>
 </template>
 
 <script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
 import { ref, reactive } from 'vue'
 const count = ref(7)
 const user = ref('')
