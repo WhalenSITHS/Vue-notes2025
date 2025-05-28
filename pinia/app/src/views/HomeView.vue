8463850b97bfa2407585ff119da7b28e664f3815
mr. whalen <michael.whalen@sitechhs.com>
2025-05-28 08:40:50 -0400

    ready

---
+++ pinia/app/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <button @click="login">Log me in</button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+
+const router = useRouter()
+const auth = useAuthStore()
+
+function login() {
+  auth.login()
+  router.push('/about')
+}
+</script>
